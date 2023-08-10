1edcb8566bd9285fefd0475cdd01f67948e4af68
Husni Faiz <husni.faiz@3mdeb.com>
2023-08-11 01:36:03 +0530

    bootstrap.sh: add option for apt packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-sudo dnf install ansible
-ansible-galaxy install geerlingguy.docker
+case "${1}" in
+  (--apt|--deb)
+    export dist_type=Debian
+    sudo apt install ansible
+    ansible-galaxy install geerlingguy.docker
+   ;;
+  (''|*)
+    export dist_type=Fedora
+    sudo dnf install ansible
+    ansible-galaxy install geerlingguy.docker
+   ;;
+esac
